48beacb9457addf8617469af99d6907c598bf413
shahd alnaami <shahdno2001@gmail.com>
2024-01-16 03:21:39 +0200

    ui test for brand page navigation

---
+++ logic/Browser/BrandPage.ts
@@ -0,0 +1,18 @@
+import { BasePage } from "../../infra/base-page";
+import { Locator, Page } from "playwright";
+
+export class BrandPage extends BasePage {
+
+    private brandIcon : Locator
+
+    constructor(page: Page) {
+        super(page)
+        this.brandIcon = page.getByAltText('adidas')
+        this.initPage()
+    }
+    brandClick = async () => {
+        await this.brandIcon.click()
+
+
+    }
+}
