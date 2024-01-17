1edf81a302d12e74c317bc49b2e56c1022631ed2
AlaaBsoul <alaabso@m.telhai.ac.il>
2024-01-17 19:42:19 +0200

    new update

--- logic/api/address-Api.ts
+++
@@ -1,7 +0,0 @@
-import { apiPostMethod } from "../../infra/api/api-request";
-import { AccountAddress } from "../Api/request-body/address-api-request"
-import urlsConfig from "../../configFiles/urls.json"
-
-export const addAddress = async ( addAddressData: AccountAddress)=>{
-    return await apiPostMethod(urlsConfig.apiUrl.addressUrl, addAddressData)
-}
--- tests/address-test.spec.ts
+++ tests/address-test.spec.ts
@@ -2,7 +2,7 @@ import { test, Page, expect } from '@playwright/test'
 import { buildAddressRequest } from '../logic/Api/request-body/address-api-request';
 import { AddressPage } from '../logic/Browser/address-page';
 import { BrowserWrapper } from "../infra/browser/browser-wrapper";
-import { addAddress } from '../logic/Api/address-Api';
+import { ApiCalls } from "../logic/Api/api-request";
 import { DeleteAddress } from '../logic/Browser/delete-address';
 import urls from "../configFiles/urls.json"
 import users from "../configFiles/user-details.json"
@@ -26,8 +26,8 @@ test.describe('Add Address Test', () => {
         const { firstname, lastname, postcode, telephone, city, country_id } = { ...users.address }
         const { streetName, streetNumber, homeNumber } = { ...users.address.street }
         const addressData = buildAddressRequest(firstname, lastname, postcode, telephone, city, country_id, { streetName, streetNumber, homeNumber })
-
-        await addAddress(addressData);
+        const api = new ApiCalls()
+        await api.addAddress(addressData);
 
         page = await browser.getPage(urls.uiUrl.address)
         const addressPage = new AddressPage(page)
