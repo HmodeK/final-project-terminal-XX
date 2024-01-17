ab0998382a7dad9f81398098d7318f04a5e5e8be
hmode kanaan <hmodekanaan5653@gmail.com>
2024-01-17 20:11:40 +0200

    try

--- logic/api/address-Api.ts
+++ logic/api/address-Api.ts
@@ -1,5 +1,5 @@
 import { apiPostMethod } from "../../infra/api/api-request";
-import { AccountAddress } from "../Api/request-body/address-api-request"
+import { AccountAddress } from "../api/request-body/address-api-request"
 import urlsConfig from "../../configFiles/urls.json"
 
 export const addAddress = async ( addAddressData: AccountAddress)=>{
--- tests/address-test.spec.ts
+++ tests/address-test.spec.ts
@@ -1,8 +1,8 @@
 import { test, Page, expect } from '@playwright/test'
-import { buildAddressRequest } from '../logic/Api/request-body/address-api-request';
+import { buildAddressRequest } from '../logic/api/request-body/address-api-request';
 import { AddressPage } from '../logic/Browser/address-page';
 import { BrowserWrapper } from "../infra/browser/browser-wrapper";
-import { addAddress } from '../logic/Api/address-Api';
+import { apiCalls } from '../logic/api/address-Api';
 import { DeleteAddress } from '../logic/Browser/delete-address';
 import urls from "../configFiles/urls.json"
 import users from "../configFiles/user-details.json"
@@ -23,8 +23,8 @@ test.describe('Add Address Test', () => {
     })
 
     test("Adding address", async ({ }) => {
-        const { firstname, lastname, postcode, telephone, city, country_id } = { ...users.address }
-        const { streetName, streetNumber, homeNumber } = { ...users.address.street }
+        const { firstname, lastname, postcode, telephone, city, country_id } = { ...users.address1 }
+        const { streetName, streetNumber, homeNumber } = { ...users.address1.street }
         const addressData = buildAddressRequest(firstname, lastname, postcode, telephone, city, country_id, { streetName, streetNumber, homeNumber })
 
         await addAddress(addressData);
