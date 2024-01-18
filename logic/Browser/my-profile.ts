7935704e6fdc64b892e2e64845b1abd57fac0a81
AlaaBsoul <alaabso@m.telhai.ac.il>
2024-01-18 11:44:37 +0200

    I swear this is the last update

--- logic/Browser/my-profile.ts
+++ logic/Browser/my-profile.ts
@@ -5,12 +5,12 @@ import { BasePage } from "../../infra/browser/base-page";
 export class MyProfile extends BasePage {
 
     private profileIcon: Locator
-    private MyInfo: Locator
+    private myInfo: Locator
 
     constructor(page: Page) {
         super(page)
         this.profileIcon = page.locator('[class="profile_3CiC rtl_6mEj"]')
-        this.MyInfo = page.getByText('הפרטים שלי')
+        this.myInfo = page.locator('//ul[@class="list_N0wM rtl_1phs"]//li')
         this.initPage()
     }
 
@@ -18,7 +18,7 @@ export class MyProfile extends BasePage {
         await this.profileIcon.click()
     }
     myInfoClick = async () => {
-        await this.MyInfo.click()
+        await this.myInfo.nth(2).click()
     }
     myInfoContent = async () => {
         return await this.page.textContent('body');
